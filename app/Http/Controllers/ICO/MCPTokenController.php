0188b9c89e52e86fdb4a2f4877796e8276b40e64
KB <you@example.com>
2024-03-02 10:01:14 +0300

    with out comment

--- app/Http/Controllers/ICO/MCPTokenController.php
+++ app/Http/Controllers/ICO/MCPTokenController.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\Emission;
 use Illuminate\Support\Facades\App;
 
+
 class MCPTokenController extends Controller
 {
     public function index(){
         $MCPtoken = Emission::where('token_name','MCP')->first() or App::abort(404);
+
         $ethPrice = json_decode(file_get_contents('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR'));
+
+
 
         return view('ico.ico_main',compact('MCPtoken','ethPrice'));
     }
